133339bd515faadb4819b8545c49d80fe8caba0e
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-04 16:43:52 +0000

    Initial Commit

---
+++ WeeklyCalendarClone/WeeklyCalendarCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyCalendarCloneApp.swift
+//  WeeklyCalendarClone
+//
+//  Created by Kyungyun Lee on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyCalendarCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
